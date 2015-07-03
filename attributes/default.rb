48ca6fd90e7b96408d9266991638590e75960ea8
Irving Popovetsky <irving@getchef.com>
2015-07-02 17:04:54 -0700

    store the version number, makes it easy to twiddle for upgrade testing

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,7 @@ default['riak-cluster']['use_interface'] = 'eth0'
 
 default['riak']['config']['listener']['http']['internal'] = "0.0.0.0:8098"
 default['riak']['config']['listener']['protobuf']['internal'] = "0.0.0.0:8087"
+
+default['riak']['package']['version']['major'] = '2'
+default['riak']['package']['version']['minor'] = '1'
+default['riak']['package']['version']['incremental'] = '1'
